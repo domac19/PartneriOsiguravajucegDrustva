0c5f78751a0f8c40124533cf6c1c20db3a3f8bb3
Domagoj Licitar <licitar.domagoj@gmail.com>
2024-02-28 16:15:42 +0100

    Added Dapper and One model with Partners

---
+++ PartneriOsiguravajućegDruštva/Models/Partner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartneriOsiguravajućegDruštva.Models
+{
+    public class Partner
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public int PartnerNumber { get; set; }
+
+        public int CroatianPIN { get; set; }
+        
+        public int PartnerTypeId { get; set; }
+        
+        public DateTime CreatedAtUtc { get; set; }
+        
+        public string CreateByUser { get; set; }
+        
+        public bool IsForeign { get; set; }
+        
+        public int ExternalCode { get; set; }
+        
+        public byte Gender { get; set; }
+        
+        public string BrojPolice { get; set; }
+        
+        public decimal IznosPolice { get; set; }
+    }
+}
